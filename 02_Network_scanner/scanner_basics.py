f6df5126bd5d7ea679522a60254061e6ffaece6a
l0neaadil <loneaadil008@outlook.com>
2020-11-15 22:54:31 +0530

    introduction to network scanner

--- 02_Network_scanner/scanner_basics.py
+++ 02_Network_scanner/scanner_basics.py
@@ -9,11 +9,18 @@ def scan(ip):
     Ethernet_packet = scapy.Ether()
     Ethernet_packet.show()
 
-    arp_packet = scapy.ARP(pdst=ip)
+    arp_packet = scapy.ARP(pdst=ip)     
+    # OR
+    # arp_packet = scapy.ARP()
+    # arp_packet.pdst = ip
+    
     print(arp_packet.summary())
     arp_packet.show()
 
     Ethernet_packet = scapy.Ether(dst='ff:ff:ff:ff:ff:bb')
+    # OR
+    # Ethernet_packet = scapy.Ether()
+    # Ethernet_packet.dst = 'ff:ff:ff:ff:ff:bb'
     print(Ethernet_packet.summary())
     Ethernet_packet.show()
 
